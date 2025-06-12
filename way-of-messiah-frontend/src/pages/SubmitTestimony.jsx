d7984661e38e309a02d5369759b57bf0ccc608ba
Yahman <deman67@protonmail.com>
2025-06-12 14:32:29 -0400

    modified the code in the SubmitTestimony.jsx file.

--- way-of-messiah-frontend/src/pages/SubmitTestimony.jsx
+++ way-of-messiah-frontend/src/pages/SubmitTestimony.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const navigate = useNavigate();
+
 export default function SubmitTestimony() {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: "",
     message: "",
