20b7f9cb28aadde18037cf79668fd205b924ef2d
Yahman <deman67@protonmail.com>
2025-06-12 12:14:30 -0400

    modified the main.jsx file adding the route for the Thank You page.

--- way-of-messiah-frontend/src/main.jsx
+++ way-of-messiah-frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SubmitTestimony from "./pages/SubmitTestimony.jsx";
 import AdminPage from "./pages/AdminPage.jsx";
+import ThankYou from "./pages/ThankYou.jsx"; // ← add this line
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,6 +13,8 @@ root.render(
       <Routes>
         <Route path="/" element={<SubmitTestimony />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/thank-you" element={<ThankYou />} />{" "}
+        {/* ← add this line */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
