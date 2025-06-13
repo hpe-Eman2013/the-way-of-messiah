d236f9420f4865b541168f77736a59dfec9e0fb8
Yahman <deman67@protonmail.com>
2025-06-13 13:04:55 -0400

    made corrections in the main.jsx file.

--- way-of-messiah-frontend/src/main.jsx
+++ way-of-messiah-frontend/src/main.jsx
@@ -14,8 +14,8 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/testimonies" element={<TestimoniesPage />} />
         <Route path="/" element={<SubmitTestimony />} />
+        <Route path="/testimonies" element={<TestimoniesPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/thank-you" element={<ThankYou />} />
       </Routes>
