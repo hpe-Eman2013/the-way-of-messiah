f193a2dbdc555a50ae984b058afbf0c004b87138
Yahman <deman67@protonmail.com>
2025-06-13 17:13:30 -0400

    corrected the path to the App.js file in main.jsx

--- way-of-messiah-frontend/src/main.jsx
+++ way-of-messiah-frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../App"; // import the main App component
+import App from "./App"; // import the main App component
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
