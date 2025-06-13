9dbe2092653a305b2e14e8fd7db3f15e8569bcb9
Yahman <deman67@protonmail.com>
2025-06-13 15:55:25 -0400

    removed index.css import statement from main.jsx which was throwing an error.

--- way-of-messiah-frontend/src/main.jsx
+++ way-of-messiah-frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // import the main App component
-import "./index.css"; // (optional) import global styles
+import App from "../App"; // import the main App component
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
