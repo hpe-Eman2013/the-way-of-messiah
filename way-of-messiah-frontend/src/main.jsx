fb205f6e02f81f485651c593a8fa6fa7b17b449c
Yahman <deman67@protonmail.com>
2025-06-13 13:30:59 -0400

    reverted the main.jsx file back to an earlier time.

--- way-of-messiah-frontend/src/main.jsx
+++ way-of-messiah-frontend/src/main.jsx
@@ -6,19 +6,17 @@ import SubmitTestimony from "./pages/SubmitTestimony.jsx";
 import AdminPage from "./pages/AdminPage.jsx";
 import ThankYou from "./pages/ThankYou.jsx"; // ← add this line
 import Home from "./pages/Home.jsx";
-import TestimoniesPage from "./pages/TestimoniesPage.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<SubmitTestimony />} />
-        <Route path="/testimonies" element={<TestimoniesPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<SubmitTestimony />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
 );
