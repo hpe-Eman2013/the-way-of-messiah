c7f1beea602fb4a22d2fef5a1289ec9bff7d53ad
Yahman <deman67@protonmail.com>
2025-06-13 11:39:27 -0400

    removed the AppRoutes function and useLocation function from the App.js file.

--- way-of-messiah-frontend/App.js
+++ way-of-messiah-frontend/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import SubmitTestimony from "./pages/SubmitTestimony";
 import Layout from "./components/Layout";
@@ -12,27 +7,18 @@ import ThankYou from "./pages/ThankYou";
 import Home from "./pages/Home";
 import Testimonies from "./pages/Testimonies";
 
-function AppRoutes() {
-  const location = useLocation();
-  console.log("Current route:", location.pathname);
-
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/testimonies" element={<Testimonies />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/submit-testimony" element={<SubmitTestimony />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-      </Routes>
-    </Layout>
-  );
-}
-
 function App() {
   return (
     <Router>
-      <AppRoutes />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/testimonies" element={<Testimonies />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/submit-testimony" element={<SubmitTestimony />} />
+          <Route path="/thank-you" element={<ThankYou />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
