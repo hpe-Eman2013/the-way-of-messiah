591b941eed4ec12ecbc5aa2eb76531885f76ae0a
Yahman <deman67@protonmail.com>
2025-06-12 21:52:15 -0400

    modified the App.js file separating the AppRoutes function from the App function.

--- way-of-messiah-frontend/App.js
+++ way-of-messiah-frontend/App.js
@@ -1,24 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import SubmitTestimony from "./pages/SubmitTestimony";
 import Layout from "./components/Layout";
 import ThankYou from "./pages/ThankYou";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Testimonies from "./pages/Testimonies";
+
+function AppRoutes() {
+  const location = useLocation();
+  console.log("Current route:", location.pathname);
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/testimonies" element={<Testimonies />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/submit-testimony" element={<SubmitTestimony />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </Layout>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/testimonies" element={<Testimonies />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/submit-testimony" element={<SubmitTestimony />} />
-          <Route path="/thank-you" element={<ThankYou />} />
-        </Routes>
-      </Layout>
+      <AppRoutes />
     </Router>
   );
 }
