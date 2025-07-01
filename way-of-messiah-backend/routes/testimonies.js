ab8180605dbe0115c77e2f4dd9301a6f706e6797
Yahman <deman67@protonmail.com>
2025-07-01 08:02:15 -0400

    removed {approved: true} in testimonies.js for testing .

--- way-of-messiah-backend/routes/testimonies.js
+++ way-of-messiah-backend/routes/testimonies.js
@@ -5,7 +5,7 @@ const Testimony = require("../models/Testimony");
 // GET /testimonies - only approved ones
 router.get("/", async(req, res) => {
     try {
-        const testimonies = await Testimony.find({ approved: true }).sort({
+        const testimonies = await Testimony.find().sort({
             createdAt: -1,
         });
         res.json(testimonies);
