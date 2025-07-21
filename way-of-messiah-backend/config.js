26081116f2451efe6fb4bd64485d279d8be43e6c
Yahman <deman67@protonmail.com>
2025-07-21 15:30:21 -0400

    changed adminAuth.js to use the config.js file.

---
+++ way-of-messiah-backend/config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+  adminUsername: process.env.ADMIN_USERNAME,
+  adminPassword: process.env.ADMIN_PASSWORD,
+  jwtSecret: process.env.JWT_SECRET
+};
--- way-of-messiah-backend/routes/adminAuth.js
+++ way-of-messiah-backend/routes/adminAuth.js
@@ -5,30 +5,29 @@ const AdminUser = require("../models/AdminUser");
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey"; // store this in .env in production
+const { adminUsername, adminPassword, jwtSecret } = require("../config");
 
-const username = process.env.ADMIN_USERNAME;
-const password = process.env.ADMIN_PASSWORD;
 // Temporary Registration Route (only for setting up first admin)
 router.post("/register", async(req, res) => {
     try {
         // Validate inputs
-        if (!username || !password) {
+        if (!adminUsername || !adminPassword) {
             return res
                 .status(400)
                 .json({ error: "Username and password are required" });
         }
         // Check if user exists
-        const existingUser = await AdminUser.findOne({ username });
+        const existingUser = await AdminUser.findOne({ adminUsername });
         if (existingUser) {
             return res.status(400).json({ error: "Username already exists" });
         }
 
         // Hash password
         const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
+        const passwordHash = await bcrypt.hash(adminPassword, salt);
 
         // Save user
-        const newUser = new AdminUser({ username, passwordHash, isAdmin: true });
+        const newUser = new AdminUser({ adminUsername, passwordHash, isAdmin: true });
         await newUser.save();
         res.json({ message: "Admin created" });
     } catch (err) {
@@ -40,14 +39,14 @@ router.post("/register", async(req, res) => {
 // Login Route
 router.post("/login", async(req, res) => {
     try {
-         const user = await AdminUser.findOne({ username });
+         const user = await AdminUser.findOne({ adminUsername });
         if (!user) return res.status(401).json({ error: "Invalid credentials" });
 
         const isMatch = await bcrypt.compare(password, user.passwordHash);
         if (!isMatch) return res.status(401).json({ error: "Invalid credentials" });
 
         const token = jwt.sign({ userId: user._id, username: user.username },
-            JWT_SECRET, { expiresIn: "1h" }
+            jwtSecret, { expiresIn: "1h" }
         );
 
         res.json({ token });
