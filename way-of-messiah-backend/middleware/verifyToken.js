6f6365857e2c100c896f944709ce319d2ac3097a
Yahman <deman67@protonmail.com>
2025-07-21 20:54:20 -0400

    console.log check

--- way-of-messiah-backend/middleware/verifyToken.js
+++ way-of-messiah-backend/middleware/verifyToken.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET;
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in environment variables");
 }
