8805f9f62eca2b9aedbb8244bd8bb6b65416e126
Yahman <deman67@protonmail.com>
2025-10-13 15:50:25 -0400

    corrected the import path in AdminLogin.jsx

--- way-of-messiah-frontend/src/pages/AdminLogin.jsx
+++ way-of-messiah-frontend/src/pages/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { api } from "./lib/api"; // shared axios instance with interceptor
+import { api } from "../lib/api"; // shared axios instance with interceptor
 
 export default function AdminLogin() {
   const [username, setUsername] = useState("");
